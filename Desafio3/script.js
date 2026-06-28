3a437e2e4b1e2bef8f5dd37bf1b54cbfb9128e5f
Luiz <luiz646567@gmail.com>
2026-06-28 19:03:44 -0300

    função hide/show dos botões

--- Desafio3/script.js
+++ Desafio3/script.js
@@ -2,12 +2,17 @@ const botoes = document.querySelectorAll(".btn");
 
 botoes.forEach((botao) => {
     botao.addEventListener("click", () => {
+        
         const icone = botao.querySelector(".icone");
+        const containerPergunta = botao.closest(".perguntas");
+        const resposta = containerPergunta.querySelector(".resposta");
 
         if (icone.getAttribute("src") === "img/icon-plus.svg") {
             icone.setAttribute("src", "img/icon-minus.svg");
+            resposta.style.display = "block"; 
         } else {
             icone.setAttribute("src", "img/icon-plus.svg");
+            resposta.style.display = "none"; 
         }
     });
 });
